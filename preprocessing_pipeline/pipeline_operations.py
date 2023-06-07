a8c99f3c984f274eb9e4955f9430c2a3039f1c62
tocontactyash <yash.puwa@gmail.com>
2023-06-07 01:20:34 -0400

    Restructuring Code + modifying json structure

--- preprocessing_pipeline/pipeline_operations.py
+++ preprocessing_pipeline/pipeline_operations.py
@@ -3,8 +3,8 @@ import uuid
 import rocksdict
 import json
 import numpy as np
-import pdftotext
-from typing import Dict, List, Optional
+from pdfminer.high_level import extract_text
+from typing import Dict, List, Optional, Union
 from dataclasses import dataclass, asdict, field
 from nltk.tokenize import sent_tokenize
 import sentence_transformers
@@ -12,36 +12,38 @@ import sentence_transformers
 model = sentence_transformers.SentenceTransformer('all-MiniLM-L6-v2')
 
 @dataclass
-class Node:
-    id: str
-    content: str
-    attributes: Dict[str, 'Attribute']
-    edges: List[str] = field(default_factory=list)
-
-@dataclass
 class Attribute:
     name: str
     value: str
     type: str
+
+@dataclass
+class Edge:
     source: str
-    
+    target: str
+    type: str
+    attributes: Dict[str, Attribute]
+
+@dataclass
+class Node:
+    id: str
+    content: str
+    type: str
+    attributes: Dict[str, Attribute]
+    edges: List[Edge]
+    parent_id: Union[str, None] = None
+
 def load_document(file_path):
     _, file_extension = os.path.splitext(file_path)
     file_extension = file_extension.lower()
 
     if file_extension == '.pdf':
         return load_pdf(file_path)
-    elif file_extension == '.html':
-        pass
-        #return load_html(file_path)
     else:
         raise NotImplementedError(f"Loading documents of type {file_extension} is not supported.")
 
 def load_pdf(file_path):
-    with open(file_path, 'rb') as file:
-        pdf_reader = pdftotext.PDF(file)
-        text = "\n\n".join(pdf_reader)
-        return text
+    return extract_text(file_path)
 
 def split_text_into_chunks(text, max_chunk_size=100):
     sentences = sent_tokenize(text)
@@ -58,16 +60,20 @@ def split_text_into_chunks(text, max_chunk_size=100):
     return chunks
 
 def store_chunks_in_db(chunks, db_path, embeds_folder_path):
-    os.makedirs(embeds_folder_path, exist_ok=True)  # create the folder if it doesn't exist
+    os.makedirs(embeds_folder_path, exist_ok=True)
     db = rocksdict.Rdict(db_path)
     prev_node_id = None
     for i, chunk in enumerate(chunks, start=1):
         node_id = str(uuid.uuid4())
-        attributes = {}  # replace this with your attributes extraction logic
+        attributes = {}  # TODO: Add your attributes extraction logic here
+        edges = []  # TODO: Add your edges generation logic here
         node = Node(
             id=node_id,
             content=chunk,
-            attributes=attributes
+            type='text',  # TODO: Update this based on your content type detection logic
+            attributes=attributes,
+            edges=edges,
+            parent_id=prev_node_id  # Connect to the previous node
         )
         
         node_dict = {'node': asdict(node)}
@@ -76,7 +82,7 @@ def store_chunks_in_db(chunks, db_path, embeds_folder_path):
         if prev_node_id is not None:
             parent_node_dict = json.loads(db[prev_node_id].decode('utf-8'))
             parent_node = Node(**parent_node_dict['node'])
-            parent_node.edges.append(node_id)
+            parent_node.edges.append(Edge(source=parent_node.id, target=node_id, type='child', attributes={}))
             db[prev_node_id] = json.dumps({'node': asdict(parent_node)}).encode('utf-8')
 
         prev_node_id = node_id
@@ -87,30 +93,23 @@ def embed_chunks(db_path, embeds_folder_path):
     
     db = rocksdict.Rdict(db_path)
     for k, v in db.items():
-        node_dict = json.loads(v.decode('utf-8'))['node']  # no 'embed' in the dictionary anymore
+        node_dict = json.loads(v.decode('utf-8'))['node'] 
         node = Node(**node_dict)
 
         embedding = model.encode(node.content)
-        np.save(f"{embeds_folder_path}/{k}.npy", embedding)  # save embeddings as .npy files
+        np.save(f"{embeds_folder_path}/{k}.npy", embedding) 
         
         node_dict = {'node': asdict(node)}
         assert 'embedding' not in node_dict['node'].keys(), 'Embedding found in node dictionary.'
 
 def execute_pipeline(document_filepath, db_filepath, embeds_folder_path):
-    # 1. Load Document
     text = load_document(document_filepath)
-    # 2. Index document into chunks
     chunks = split_text_into_chunks(text)
-    # 3. Store grpah in rocsDB
     store_chunks_in_db(chunks, db_filepath, embeds_folder_path)
-    # 4. Generate vector embeds fro chunks and store in vectore store
     embed_chunks(db_filepath, embeds_folder_path)
 
-# Specify the file paths
 document_filepath = 'preprocessing_pipeline/sample.pdf'
 db_filepath = 'preprocessing_pipeline/test_dict'
 embeds_folder_path = 'preprocessing_pipeline/vector_store'
 
-# Execute the pipeline
 execute_pipeline(document_filepath, db_filepath, embeds_folder_path)
-
--- preprocessing_pipeline/readDB.py
+++ preprocessing_pipeline/readDB.py
@@ -2,15 +2,8 @@ import rocksdict
 import numpy as np
 import json
 from dataclasses import asdict, field
-from typing import Dict, List, Optional
+from typing import Dict, List, Optional, Union
 from dataclasses import dataclass
-
-@dataclass
-class Node:
-    id: str
-    content: str
-    attributes: Dict[str, 'Attribute']
-    edges: List[str] = field(default_factory=list)
 
 @dataclass
 class Attribute:
@@ -18,19 +11,35 @@ class Attribute:
     value: str
     type: str
 
+@dataclass
+class Edge:
+    source: str
+    target: str
+    type: str
+    attributes: Dict[str, Attribute]
+
+@dataclass
+class Node:
+    id: str
+    content: str
+    type: str
+    attributes: Dict[str, Attribute]
+    edges: List[Edge]
+    parent_id: Union[str, None] = None
+
 def read_from_db(db_path, json_file_path):
     db = rocksdict.Rdict(db_path)
     data_to_export = {}
     for k, v in db.items():
         print(f"Raw output from RocksDB: {v}")
-        print("Attempting to load JSON string")
+        # print("Attempting to load JSON string")
         node_dict = json.loads(v.decode('utf-8'))['node']  # no 'embed' in the dictionary anymore
         node = Node(**node_dict)
         data_to_export[k] = asdict(node)
-        print(f'Node ID: {k}')
-        print(f'Content: {node.content}')
-        print(f'Attributes: {node.attributes}')
-        print(f'Edges: {node.edges}\n')
+        # print(f'Node ID: {k}')
+        # print(f'Content: {node.content}')
+        # print(f'Attributes: {node.attributes}')
+        # print(f'Edges: {node.edges}\n')
 
     with open(json_file_path, 'w') as json_file:
         json.dump(data_to_export, json_file)
@@ -40,4 +49,4 @@ def read_from_db(db_path, json_file_path):
 # Example usage
 db_filepath = 'preprocessing_pipeline/test_dict'
 json_file_path = 'preprocessing_pipeline/db_export.json'
-read_from_db(db_filepath, json_file_path) 
+read_from_db(db_filepath, json_file_path)
