b2a300c2773c9ca8cbe8994f53464a535d324c3d
raketlak <raketla.k@northeastern.com>
2023-06-14 15:55:30 +0530

    create raw and structured jsons

--- preprocessing_pipeline/execute_pipeline.py
+++ preprocessing_pipeline/execute_pipeline.py
@@ -1,6 +1,6 @@
 from document_loader import split_text_into_chunks, load_document
 from pipeline_operations import store_chunks_in_graph, embed_chunks
-from readDB import read_from_db
+from readDB import *
 
 document_filepaths = ['preprocessing_pipeline/documents/complex.pdf', 'preprocessing_pipeline/documents/sample.pdf'] 
 db_filepath = 'preprocessing_pipeline/output_files/test_dict'
@@ -19,5 +19,8 @@ def execute_pipeline(document_filepaths, db_filepath, embeds_folder_path, max_ch
 
 if __name__ == '__main__':
     execute_pipeline(document_filepaths, db_filepath, embeds_folder_path, max_chunk_size=100)
-    json_file_path = 'preprocessing_pipeline/output_files/db_export.json'
-    read_from_db(db_filepath, json_file_path)
+    jjson_file_path_raw = "preprocessing_pipeline/output_files/db_export_raw.json"
+    json_file_path_structured = "preprocessing_pipeline/output_files/db_export_structured.json"
+    db_filepath = 'preprocessing_pipeline/output_files/test_dict'
+    read_all_Edges_Vertices(db_filepath, jjson_file_path_raw)
+    generate_json(db_filepath, json_file_path_structured)
